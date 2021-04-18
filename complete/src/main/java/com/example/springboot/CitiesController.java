7589002f0c035f8610413a457de3721e73397e89
Juan <juan.gonzalez1@udc.es>
2021-04-18 15:52:37 +0200

    change

--- complete/src/main/java/com/example/springboot/CitiesController.java
+++ complete/src/main/java/com/example/springboot/CitiesController.java
@@ -10,21 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CitiesController {
 
 	private static final ArrayList<Cities> cities = new ArrayList<Cities>();
-	cities.add(new Cities("1","Ourense","Ourense"));
-	cities.add(new Cities("2","Coruna","Coruna"));
-	cities.add(new Cities("3","Pontevedra","Pontevedra"));
-	cities.add(new Cities("4","Santiago","Coruna"));
-	cities.add(new Cities("5","Lugo","Lugo"));
-	cities.add(new Cities("5","Vigo","Pontevedra"));
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/cities")
 	public Cities cities() {
-		return cities;
+		return new Cities("1", "Ourense","Ourense");
 	}
 
 	@GetMapping("/cities/{id}")
 	public Greeting greeting(@PathVariable Long id) {
-		return cities.get(id-1);
+		return new Cities("1", "Ourense","Ourense");
 	}
 }
