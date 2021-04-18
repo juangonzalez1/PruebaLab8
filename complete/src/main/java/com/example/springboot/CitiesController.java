c18bef85993a3cca0b0176d94a1f75316cc4daf8
Juan <juan.gonzalez1@udc.es>
2021-04-18 22:36:32 +0200

    change

--- complete/src/main/java/com/example/springboot/CitiesController.java
+++ complete/src/main/java/com/example/springboot/CitiesController.java
@@ -12,12 +12,7 @@ public class CitiesController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 	private static final ArrayList<Cities> citieslista = new ArrayList<Cities>();
-	citieslista.add(1,"Ourense","Ourense");
-	citieslista.add(2,"A coruña","Ourense");
-	citieslista.add(3,"Pontevedra","Ourense");
-	citieslista.add(4,"Lugo","Ourense");
-	citieslista.add(5,"Vigo","Ourense");
-
+	citieslista.add(new Cities(1,"Ourense","Ourense"));
 
 	@GetMapping("/cities")
 	public Cities cities(@RequestParam(value = "name", defaultValue = "World") String name) {
