39d8222f56745161c04531a8327854a45ab834e8
Juan <juan.gonzalez1@udc.es>
2021-04-24 17:14:47 +0200

    change

--- complete/src/test/java/com/example/springboot/HelloControllerTest.java
+++ complete/src/test/java/com/example/springboot/HelloControllerTest.java
@@ -26,4 +26,11 @@ public class HelloControllerTest {
 				.andExpect(status().isOk())
 				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
 	}
+
+	@Test
+	public void getCities() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/city").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("[{\"id\":1,\"name\":\"Ourense\",\"province\":\"Ourense\"},{\"id\":2,\"name\":\"Vigo\",\"province\":\"Pontevedra\"},{\"id\":3,\"name\":\"Lugo\",\"province\":\"Lugo\"},{\"id\":4,\"name\":\"Coru\u00f1a\",\"province\":\"Coru\u00f1a\"},{\"id\":5,\"name\":\"Santiago\",\"province\":\"Coru\u00f1a\"}]")));
+	}
 }
